67333cd4d23ded3311bcf3310fc4114135c44320
Mikaela-D <G00401392@atu.ie>
2024-12-09 21:58:34 +0000

    Adjusted text

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,23 +1,26 @@
-import classes from './MainNavigation.module.css'
-import Link from 'next/link'
-import HamMenu from "../generic/HamMenu"
-import HamMenuFAB from "../generic/HamMenuFAB"
-import { useContext } from 'react'
-import GlobalContext from "../../pages/store/globalContext"
-import HamMenuContent from "./HamMenuContent"
-import { useRouter } from 'next/router'
+import classes from "./MainNavigation.module.css";
+import Link from "next/link";
+import HamMenu from "../generic/HamMenu";
+import HamMenuFAB from "../generic/HamMenuFAB";
+import { useContext } from "react";
+import GlobalContext from "../../pages/store/globalContext";
+import HamMenuContent from "./HamMenuContent";
+import { useRouter } from "next/router";
 
 function MainNavigation() {
-  const globalCtx = useContext(GlobalContext)
-  const router = useRouter()
+  const globalCtx = useContext(GlobalContext);
+  const router = useRouter();
 
   function toggleMenuHide() {
-    globalCtx.updateGlobals({ cmd: 'hideHamMenu', newVal: false })
+    globalCtx.updateGlobals({ cmd: "hideHamMenu", newVal: false });
   }
 
-  const contents = []
-  globalCtx.theGlobalObject.meetings.forEach(element => {
-    contents.push({title: element.title, webAddress: '/' + element.meetingId })
+  const contents = [];
+  globalCtx.theGlobalObject.meetings.forEach((element) => {
+    contents.push({
+      title: element.title,
+      webAddress: "/" + element.meetingId,
+    });
   });
 
   return (
@@ -28,10 +31,11 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link href='/'>All Meetups</Link> ({globalCtx.theGlobalObject.meetings.length})
+            <Link href="/">All Products</Link> (
+            {globalCtx.theGlobalObject.meetings.length})
           </li>
           <li>
-            <Link href='/new-meetup'>Add New Meetup</Link>
+            <Link href="/new-meetup">Add New Product</Link>
           </li>
         </ul>
       </nav>
@@ -39,4 +43,4 @@ function MainNavigation() {
   );
 }
 
-export default MainNavigation
+export default MainNavigation;

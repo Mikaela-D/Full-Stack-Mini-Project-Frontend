d83a9884ead58c8743ae3214e6dc49974cf66c64
Mikaela-D <G00401392@atu.ie>
2024-12-10 04:17:59 +0000

    Improved the popup css

--- components/generic/ProductsPopup.js
+++ components/generic/ProductsPopup.js
@@ -1,9 +1,29 @@
 import classes from "./ProductsPopup.module.css";
+import { useCart } from "../generic/CartContext";
+import { useRouter } from "next/router";
+import MeetupId from "../../pages/[meetupId]";
 
 const ProductsPopup = ({ category, products, onClose }) => {
+  const { addToCart } = useCart();
+  const router = useRouter();
+
   const filteredProducts = products.filter(
     (product) => product.category === category
   );
+
+
+
+  // Handler to add product to cart
+  const addToCartHandler = (product) => {
+    addToCart({
+      id: product.id,
+      image: product.image,
+      title: product.title,
+      price: product.price,
+      category: product.category,
+    });
+    router.push("/cart");
+  };
 
   return (
     <div className={classes.popupBackground} onClick={onClose}>
@@ -12,15 +32,36 @@ const ProductsPopup = ({ category, products, onClose }) => {
         onClick={(e) => e.stopPropagation()}
       >
         <h2>Products in {category}</h2>
-        <ul>
-          {filteredProducts.map((product) => (
-            <li key={product.id} className={classes.productItem}>
-              <img src={product.image} alt={product.title} />
-              <h3>{product.title}</h3>
-              <p>{product.price}€</p>
-            </li>
-          ))}
-        </ul>
+        {filteredProducts.length > 0 ? (
+          <ul className={classes.productList}>
+            {filteredProducts.map((product) => (
+              <li key={product.id} className={classes.productItem}>
+                <div className={classes.productDetails}>
+                  <img src={product.image} alt={product.title} />
+                  <div className={classes.productInfo}>
+                    <h3>{product.title}</h3>
+                    <p>
+                      <strong>Price:</strong> {product.price}€
+                    </p>
+                    <p>
+                      <strong>Description:</strong> {product.description}
+                    </p>
+                    <div className={classes.actions}>
+    
+                      <button
+                        onClick={() => addToCartHandler(product)}
+                      >
+                        Add to Cart
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No products available in this category.</p>
+        )}
         <button onClick={onClose} className={classes.closeButton}>
           Close
         </button>

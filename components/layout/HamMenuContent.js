ffa8b6fa031ffb5a1dc5496b764b3927cf1b138d
Mikaela-D <G00401392@atu.ie>
2024-12-10 03:43:56 +0000

    Popup works but menu is constantly open

--- components/layout/HamMenuContent.js
+++ components/layout/HamMenuContent.js
@@ -1,40 +1,51 @@
-import classes from './HamMenuContent.module.css'
-import { useRouter } from 'next/router'
-import { useContext, useState } from 'react'
-import GlobalContext from "../../pages/store/globalContext"
+import classes from "./HamMenuContent.module.css";
+import { useState, useContext } from "react";
+import GlobalContext from "../../pages/store/globalContext";
+import ProductsPopup from "../generic/ProductsPopup";
 
-export default function HamMenuContent(props) {
-    const globalCtx = useContext(GlobalContext)
-    const router = useRouter()
-    let [popupToggle, setPopupToggle] = useState(false)
+export default function HamMenuContent({ onClose }) {
+  const globalCtx = useContext(GlobalContext);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-    if (globalCtx.theGlobalObject.hideHamMenu) {
-        return null
-    }
+  // Define categories
+  const categories = ["Food", "Clothes", "Furniture"];
 
-    function clicked(webAddress) {
-        globalCtx.updateGlobals({ cmd: 'hideHamMenu', newVal: true })
-        router.push(webAddress)
-    }
+  // Handle category click to show popup
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
 
-    function closeMe() {
-        globalCtx.updateGlobals({ cmd: 'hideHamMenu', newVal: true })
-        if (popupToggle == true) {
-            setPopupToggle(false)
-        } else {
-            setPopupToggle(true)
-        }
-    }
+  // Close the popup
+  const handleClosePopup = () => {
+    setSelectedCategory(null);
+  };
 
-    let contentJsx = props.contents.map((item, index) => (  //  [{title: 'Meeting 1', webAddress: '/meet1'}, {title: 'Meeting 2', webAddress: '/meet2'}]
-        <div className={classes.menuItem} key={index} onClick={() => clicked(item.webAddress)} >{item.title} </div>
-    ))
-
-    return (
-        <div className={classes.background} onClick={() => closeMe()} >
-            <div className={classes.mainContent} >
-                {contentJsx}
+  return (
+    <>
+      {/* This div is the background that closes the menu when clicked */}
+      <div className={classes.background} onClick={onClose}>
+        <div className={classes.mainContent}>
+          {/* Display categories */}
+          {categories.map((category, index) => (
+            <div
+              key={index}
+              className={classes.menuItem}
+              onClick={() => handleCategoryClick(category)} // Handle category click
+            >
+              {category}
             </div>
+          ))}
         </div>
-    );
+      </div>
+
+      {/* Show the products in the selected category if one is selected */}
+      {selectedCategory && (
+        <ProductsPopup
+          category={selectedCategory}
+          products={globalCtx.theGlobalObject.meetings}
+          onClose={handleClosePopup}
+        />
+      )}
+    </>
+  );
 }
